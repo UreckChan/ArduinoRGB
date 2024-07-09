ae56868691fb9b9e5d318de9e77211ba0ccfc40a
Uriel Gómez <urielgomezbecerril@gmail.com>
2024-07-08 20:01:17 -0600

    first commit

--- lib/widgets/colors_widget.dart
+++ lib/widgets/colors_widget.dart
@@ -9,12 +9,7 @@ class ColorsWidget extends StatefulWidget {
 }
 
 class _ColorsWidgetState extends State<ColorsWidget> {
-  Color _currentColor = Colors.green;
   bool _switchValue = false;
-  final _controller = CircleColorPickerController(
-    initialColor: Colors.green,
-  );
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,59 +19,11 @@ class _ColorsWidgetState extends State<ColorsWidget> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Center(
-            child: CircleColorPicker(
-              controller: _controller,
-              onChanged: (color) {
-                setState(() => _currentColor = color);
-                widget.sendData("R${color.red}G${color.green}B${color.blue}");
-              },
-            ),
           ),
           const SizedBox(height: 25),
           Column(
             children: [
-              Slider(
-                value: _currentColor.red.toDouble(),
-                min: 0,
-                max: 255,
-                thumbColor: Colors.red,
-                activeColor: Colors.red,
-                onChanged: (value) {
-                  setState(() {
-                    _controller.color = Color.fromRGBO(value.toInt(),
-                        _currentColor.green, _currentColor.blue, 1);
-                  });
-                  widget.sendData("R${value.toInt()}");
-                },
-              ),
-              Slider(
-                value: _currentColor.green.toDouble(),
-                min: 0,
-                max: 255,
-                thumbColor: Colors.green,
-                activeColor: Colors.green,
-                onChanged: (value) {
-                  setState(() {
-                    _controller.color = Color.fromRGBO(_currentColor.red,
-                        value.toInt(), _currentColor.blue, 1);
-                  });
-                  widget.sendData("G${value.toInt()}");
-                },
-              ),
-              Slider(
-                value: _currentColor.blue.toDouble(),
-                min: 0,
-                max: 255,
-                thumbColor: Colors.blue,
-                activeColor: Colors.blue,
-                onChanged: (value) {
-                  setState(() {
-                    _controller.color = Color.fromRGBO(_currentColor.red,
-                        _currentColor.green, value.toInt(), 1);
-                  });
-                  widget.sendData("B${value.toInt()}");
-                },
-              ),
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -93,6 +40,7 @@ class _ColorsWidgetState extends State<ColorsWidget> {
                     onChanged: (value) {
                       setState(() {
                         _switchValue = value;
+                        widget.sendData(value ? "T" : "T");
                       });
                       widget.sendData(value ? "F" : "F");
                     },
